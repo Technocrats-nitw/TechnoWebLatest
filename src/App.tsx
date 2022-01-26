5e9ae78eb203b226f6d6717d071bf08ca16d929d
Sufiyan Ansari <ansari_841903@student.nitw.ac.in>
2022-01-27 01:24:17 +0530

    Added routes and router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
 import React from 'react'
-import { Footer } from './components/Footer/Footer'
-import { Main } from './components/Main/Main'
+import { Footer } from './components/Footer'
 import { GlobalStyle } from './styles/global'
 import 'react-toastify/dist/ReactToastify.css'
+import { BrowserRouter as Router ,Routes, Route} from 'react-router-dom'
+import Team from './pages/Team';
+import Intro from './pages/Intro';
+import Projects from './pages/Projects';
+import {Container} from "./components/styles/main";
+import { Header } from "./components/Header";
+ 
 function App() {
   return (
     <>
       <GlobalStyle></GlobalStyle>
-      <Main></Main>
+      <Router>
+      <Container>
+      <Header></Header>
+      <Routes>
+          <Route path="/" element={<Intro/>}/>
+          <Route path="/team" element={<Team/>}/>
+          <Route path="/projects" element={<Projects/>}/>
+          <Route path="/getInTouch" element={<Team/>}/>
+      </Routes>
+      </Container>
+      </Router>
       <Footer/>
     </>
   )
