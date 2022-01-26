7d6b05ed4137c47f46d6e2b706ce3d61dbe5511f
Sufiyan Ansari <ansari_841903@student.nitw.ac.in>
2022-01-27 01:28:45 +0530

    Styles made with proper name in components/styles

---
+++ src/components/styles/intro.ts
@@ -0,0 +1,72 @@
+import styled from "styled-components";
+
+
+
+export const Container = styled.section`
+  padding-top: 15%;
+  display: flex;
+  justify-content: space-between;
+  gap: 8rem;
+  background: rgba(0,0,0,0);
+
+  .center {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    
+  }
+  
+  .hero-text{
+    & > p{
+      font-size: 1.8rem;
+    }
+    h1{
+      font-size: 7rem;
+      color:#432a59;
+    }
+
+    h3{
+      color:var(--purple);
+      margin: 1rem 0;
+    }
+
+    
+    p.small-resume {
+      margin-bottom: 5rem;
+    }
+  }
+
+  .button{
+    margin-top: 5rem;
+    padding: 1.4rem 6rem;
+  }
+
+  .hero-image{
+    img{
+      max-width: 500px;
+    }
+  }
+
+
+  @media(max-width: 960px){
+    display: block;
+    margin-top: 15%;
+    .hero-text{
+
+      h1{
+        font-size: 5rem;
+      }
+    }
+    
+    .hero-image{
+      display: none;
+    }
+  }
+
+  @media(max-width: 600px){
+    margin-top: 25%;
+  }
+  @media(max-width: 480px){
+    margin-top: 35%;
+  }
+`
