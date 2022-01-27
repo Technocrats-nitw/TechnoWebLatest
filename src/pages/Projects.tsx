4cf3079131347e70d0f6b6a2e714b3e265c131ff
Sufiyan Ansari <ansari_841903@student.nitw.ac.in>
2022-01-28 03:41:38 +0530

    project page modulated

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,8 +1,7 @@
 import React from "react";
 import { Container } from "../assets/styles/projects";
-import ScrollAnimation from "react-animate-on-scroll";
-import { Css, Django, Flutter, Js, Python, Tf, Torch, Unity } from "../components/Icons";
-
+import { projects } from "../constants/projects";
+import { ProjectCard } from "../components/project-card";
 
 
 function Projects(){
@@ -11,145 +10,9 @@ function Projects(){
       <h2>Project Gallery</h2>
 
       <div className="projects">
-
-      <ScrollAnimation  animateOnce={true} animateIn="flipInX">
-        <div className="project">
-          <div className="body">
-            <h3>GoVID</h3>
-            <p>Hackon With Amazon Project
-            </p>
-
-          <div className="hard-skills">
-            <Django/>
-            <Flutter/>
-            <Python/>
-            
-          </div>
-          <footer>  
-          <ul className="tech-list">
-                <li>Anshuman</li>
-                <li>Chaitanya</li>
-                <li>Ashiqa</li>
-                <li>Sufiyan</li>
-              </ul>
-          </footer>
-          </div>
-        </div>
-      </ScrollAnimation>
-
-        <ScrollAnimation  animateOnce={true} animateIn="flipInX">
-          <div className="project">
-            <div className="body">
-              <h3>Voctor 2.0</h3>
-              <p>Hackon With Amazon Project
-              </p>
-            </div>
-            <div className="hard-skills">
-              <Python/>
-              <Tf/>
-          </div>
-          </div>
-        </ScrollAnimation>
-
-        <ScrollAnimation  animateOnce={true} animateIn="flipInX">
-          <div className="project">
-            <div className="body">
-              <h3>TalkAR</h3>
-              <p>OIC NIW </p>
-            </div>
-            <div className="hard-skills">
-              
-              <Unity/>
-            </div>
-            <footer>
-              <ul className="tech-list">
-                <li>Aaditya</li>
-                <li>Ashiqa</li>
-                <li>Abdullah</li>
-                <li>Chaitanya</li>
-                <li>Sufiyan</li>
-                <li>Anshuman</li>
-              </ul>
-            </footer>
-          </div>
-        </ScrollAnimation>
-
-        <ScrollAnimation  animateOnce={true} animateIn="flipInX">
-          <div className="project">
-            <div className="body">
-              <h3>Hactoberfest 2021</h3>
-              <p>Maintained a Repository Open to Contribution
-              </p>
-            </div>
-            <footer>
-              <ul className="tech-list">
-                <li>Sufiyan</li>
-                <li>Anshuman</li>
-              </ul>
-            </footer>
-          </div>
-        </ScrollAnimation>
-
-        <ScrollAnimation  animateOnce={true} animateIn="flipInX">
-          <div className="project">
-            <div className="body">
-              <h3>PyDobot M1</h3>
-              <p>Drivers for a common robotic arm.</p>
-            </div>
-
-            <div className="hard-skills">
-              <Python/>
-            </div>
-            <footer>
-              <ul className="tech-list">
-                <li>Abdullah</li>
-              </ul>
-            </footer>
-          </div>
-        </ScrollAnimation>
-
-        <ScrollAnimation  animateOnce={true} animateIn="flipInX">
-          <div className="project">
-            <div className="body">
-              <h3>Connect</h3>
-              <p>A Blogging and Social Media Platform</p>
-            </div>
-
-            <div className="hard-skills">
-              <Django/>
-              <Js/>
-              <html/>
-              <Css/>
-            </div>
-            <footer>
-              <ul className="tech-list">
-                <li>Chaitanya Hardikar</li>
-              </ul>
-            </footer>
-          </div>
-        </ScrollAnimation>
-
-
-        <ScrollAnimation  animateOnce={true} animateIn="flipInX">
-          <div className="project">
-            <div className="body">
-              <h3>UNiT</h3>
-              <p>Reasearch Paper Implementation : Multimodal Multitask Learning with a Unified Transformer</p>
-            </div>
-
-            <div className="hard-skills">
-              <Torch/>
-              <Python/>
-            </div>
-            <footer>
-              <ul className="tech-list">
-                <li>Chaitanya Hardikar</li>
-              </ul>
-            </footer>
-          </div>
-        </ScrollAnimation>
-
-
+        {projects.map((project) =>{
+          return <ProjectCard project={project} />
+        })}
       </div>
     </Container>
   );
