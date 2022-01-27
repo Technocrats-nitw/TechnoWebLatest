570329dca550f6589369ecdacc4b56aa681a63fa
Sufiyan Ansari <ansari_841903@student.nitw.ac.in>
2022-01-28 03:40:01 +0530

    global css moved to assets/style

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Footer } from './components/Footer'
-import { GlobalStyle } from './styles/global'
+import { GlobalStyle } from './assets/styles/global'
 import 'react-toastify/dist/ReactToastify.css'
 import { BrowserRouter as Router ,Routes, Route} from 'react-router-dom'
 import Team from './pages/Team';
@@ -8,6 +8,7 @@ import Intro from './pages/Intro';
 import Projects from './pages/Projects';
 import {Container} from "./assets/styles/main";
 import { Header } from "./components/Header";
+import Testimonial from './pages/testimonial'
  
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
           <Route path="/web" element={<Intro/>}/>
           <Route path="/team" element={<Team/>}/>
           <Route path="/projects" element={<Projects/>}/>
+          <Route path="/testimonial" element={<Testimonial/>}/>
       </Routes>
       </Container>
       </Router>
